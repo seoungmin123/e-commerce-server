e5e077c2f6d4d86ee4ef814d44b82fa4dd42546a
iseungmin <sm1111176@gmail.com>
2025-07-24 20:06:14 +0900

    chore : 테스트H2 데이터베이스 연걸 , 의존성 추가 ,

--- src/main/java/kr/hhplus/be/server/ServerApplication.java
+++ src/main/java/kr/hhplus/be/server/ServerApplication.java
@@ -1,13 +1,30 @@
 package kr.hhplus.be.server;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+import java.util.Arrays;
+
+@EnableJpaRepositories(basePackages = "kr.hhplus.be.server")
+@EntityScan(basePackages = "kr.hhplus.be.server")
+@SpringBootApplication(scanBasePackages = "kr.hhplus.be.server")
 public class ServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner debugContext(ApplicationContext context) {
+		return args -> {
+			System.out.println("============== BEANS ==============");
+			Arrays.stream(context.getBeanDefinitionNames())
+					.filter(name -> name.contains("coupon"))
+					.forEach(System.out::println);
+		};
+	}
 }
